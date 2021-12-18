221b7cec7c51bdd310c4ad77534584c4b1049d2d
Astrono2 <jerescharovsky@gmail.com>
2021-12-17 22:30:34 -0300

    Updated build to use make

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-(cd core; mkdir build; scons use_PIC=True)
+(cd core; mkdir build; make static)
 gcc -o build/pypete.o -c src/petemodule.c -I/usr/include/python3.9 -Icore/include -Lcore/build -lpete -fPIC
 gcc -shared -o build/pete.so build/pypete.o -Lcore/build -lpete
